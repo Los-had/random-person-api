0f02904ea82341eeac64df8448ba065f68b3f8ef
Miguel <79226657+Los-had@users.noreply.github.com>
2021-08-08 02:25:35 +0000

    grandes features adicionadas

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
 from gerador import generate_random_person
 from time import sleep
+from enviar_email import enviar_email
 
 app = Flask(__name__)
 
@@ -29,5 +30,22 @@ def goto_index():
 @app.route('/error')
 def error():
   return render_template('error.html')
+
+@app.route('/send')
+def página_email():
+  return render_template('send.html')
+
+@app.route('/sucesso', methods=['POST', 'GET'])
+def sucesso():
+  try:
+    if request.method == 'POST':
+      msg = request.form['texto']
+      email = request.form['email']
+      enviar_email(email, msg)
+      return redirect('/')
+    else:
+      return redirect('/error')
+  except:
+    return redirect('/error')
 
 app.run(host='0.0.0.0', port=8080, debug=True)
